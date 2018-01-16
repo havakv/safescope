27047e1f2d05973754b6e10331fe816210642379
Havard <havard@Havards-MacBook-Pro.local>
2018-01-15 18:12:29 -0800

    Fixed requirements

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    're',
-    'inspect',
+    # 're',
+    # 'inspect',
 ]
 
 setup_requirements = [
