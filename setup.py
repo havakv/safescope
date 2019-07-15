2cda83a13f34cfe7ce52ab1964d7b1759ad6f814
havakv <haavard.kvamme@gmail..com>
2019-07-15 21:51:04 +0200

    Fixed correct python versions

--- setup.py
+++ setup.py
@@ -47,9 +47,11 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.5',
     ],
     test_suite='tests',
     tests_require=test_requirements,
     setup_requires=setup_requirements,
-    # python_requires='>=3.7',
+    python_requires='>=3.5',
 )
